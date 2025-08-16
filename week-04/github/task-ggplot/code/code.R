55c6692affd646763a6bc27a3b85b15136d8b5e0
freeecondata <freeecondata@gmail.com>
2025-08-16 16:08:47 -0500

    uppload clase 02

---
+++ week-04/github/task-ggplot/code/code.R
@@ -0,0 +1,24 @@
+## Nombre ( s ) de Autor ( es )
+## R version 4.5.0
+
+## limpiar entorno
+rm(list = ls())
+
+## cargar librerias
+require(dplyr)
+require(ggpplot)
+
+##==: Punto 1
+
+
+##==: Punto 2
+
+
+##==: Punto 3
+
+
+##==: Punto 4
+
+
+##==: Punto 5
+
